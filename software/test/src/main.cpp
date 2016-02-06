9c31c4cfd4f5fc7c3c5573d1773ca49c97ddd1fb
unknown <z80@cortex-m3.(none)>
2016-02-05 17:08:14 -0800

    USBXpress driver support is inplemented. But can't install device driver itself.

--- software/test/src/main.cpp
+++ software/test/src/main.cpp
@@ -1,54 +1,20 @@
 
 #include <QtGui>
 #include <QApplication>
-#include "voltamp_io.h"
-
-#include "qextserialport.h"
+#include "io.h"
 
 
 int main( int argc, char * argv[] )
 {
     QApplication app( argc, argv );
 
-    VoltampIo io;
-    bool res;
+    Io io;
     QStringList l = io.enumDevices();
     qDebug() << l;
-    res = io.open( 4 );
-    if ( !res )
-        return -1;
 
-    /*
-    quint8 v;
-    bool eom;
-    v = 234;
-    int cnt;
-    cnt = io.write( &v, 1 );
-    cnt = io.read( &v, 1, eom );
-    qDebug() << v;
-
-    for ( v=0; v<30; v++ )
-    {
-        cnt = io.write( &v, 1 );
-        cnt = io.read( &v, 1, eom );
-        qDebug() << v;
-    }
-    */
-
-    QString stri;
-
-    QVector<int> data;
-    res = io.setTriggerEn( true );
-    res = io.setDac1( 32767, 32767 );
-    res = io.setDac1( 32767, 41000 );
-    res = io.setDac1( 32767, 32767 );
-    res = io.setDac1( 32767, 41000 );
-    res = io.setDac1( 32767, 32767 );
-    res = io.setDac1( 32767, 41000 );
-    res = io.sweepData( data );
-
+    bool res = io.open( 0 );
     io.close();
-    
+
     return 0;
 }
 
